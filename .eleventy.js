2893e861ded4ec85e74bd4888217589763cdce6d
Mike Aparicio <peruvianidol@gmail.com>
2020-09-08 22:35:13 -0500

    Add logo, wordmark and layout styles

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = function(eleventyConfig) {
   // Passthrough copy
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("images");
 
   // Plugins
   eleventyConfig.addPlugin(syntaxHighlight);
